3135d9e524acec87269ea5069ae9bf513e17fc04
nathan <nathan@acmeaom.com>
2015-04-02 14:41:03 -0700

    initial commit of newrelic + jack+jill exception repro

---
+++ app/src/androidTest/java/com/example/acmeaom/march2015checkdefaultgradlebuildscript/ApplicationTest.java
@@ -0,0 +1,13 @@
+package com.example.acmeaom.march2015checkdefaultgradlebuildscript;
+
+import android.app.Application;
+import android.test.ApplicationTestCase;
+
+/**
+ * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
+ */
+public class ApplicationTest extends ApplicationTestCase<Application> {
+    public ApplicationTest() {
+        super(Application.class);
+    }
+}
